73dba5461d4554c3cc5193cb41832643f5b8160d
BettyBaneWarlock <beteldessalegn0147@gmail.com>
2025-01-09 14:25:41 +0300

    dashboard

--- inventory_management/inventory/models.py
+++ inventory_management/inventory/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class InventoryItem(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+	name = models.CharField(max_length=200)
+
+	class Meta:
+		verbose_name_plural = 'categories'
+
+	def __str__(self):
+		return self.name
